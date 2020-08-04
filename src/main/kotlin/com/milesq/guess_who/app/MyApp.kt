3fefdc08b68942bbfa6cbdfbd392f5a16c97472e
Milesq <wmilosz88@gmail.com>
2020-08-04 11:35:08 +0200

    Initial files

---
+++ src/main/kotlin/com/milesq/guess_who/app/MyApp.kt
@@ -0,0 +1,6 @@
+package com.milesq.guess_who.app
+
+import com.milesq.guess_who.view.MainView
+import tornadofx.App
+
+class MyApp : App(MainView::class, Styles::class)
---
+++ src/main/kotlin/com/milesq/guess_who/view/MainView.kt
@@ -0,0 +1,12 @@
+package com.milesq.guess_who.view
+
+import com.milesq.guess_who.app.Styles
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+    override val root = hbox {
+        label(title) {
+            addClass(Styles.heading)
+        }
+    }
+}
